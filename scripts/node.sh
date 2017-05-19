258bdd81b5a654626fd160194bbb20f1b619b3ab
Chintamani Lonkar <Mani@Mani-MacBook.local>
2017-05-19 11:51:14 -0700

    added nginx updated Vagrant

--- scripts/node.sh
+++
@@ -1,6 +0,0 @@
-cd;
-echo "Installing node.js & npm"
-curl -sL https://rpm.nodesource.com/setup | sudo bash -;
-yum install -y nodejs;
-yum install -y gcc-c++ openssl-devel make; #Optional but useful.
-npm install jasmine-node -g; #For testing.
